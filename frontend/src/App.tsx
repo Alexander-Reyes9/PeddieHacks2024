e355edca0573c04b7af8b84dbfc869809a072ec5
baker-1234 <bakerjbook@gmail.com>
2024-08-17 00:19:46 -0700

    Initial commit with vite template and tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,35 @@
 import { useState } from 'react'
-import spotifyLogo from './assets/spotify_logo.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
-async function fetchApi(playlistLink: string) {
-    const response = await fetch('http://127.0.0.1:5000/get_playlist_emotion', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ playlistLink }),
-    });
-    const data = await response.json();
-    return data.emotion;
-}
-
-function EmotionDisplay({ emotion }: { emotion: string | null }){
-    if (!emotion){
-        return (
-            null
-        )
-    }else{
-        return (
-            <h2>{emotion}</h2>
-        )
-    }
-}
-
 function App() {
-    const [emotion, setEmotion] = useState<string | null>(null)
+  const [count, setCount] = useState(0)
 
-    return (
-        <div className="grid place-items-center">
-            <img className="logo select-none" src={spotifyLogo}></img>
-            <h1>Song Emotion Website</h1>
-            <input type="text" id="playlistLink" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 m-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Playlist link" />
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={async () => {
-                let input = document.getElementById("playlistLink") as HTMLInputElement
-                setEmotion(await fetchApi(input.value))
-            }}>Submit</button>
-            <EmotionDisplay emotion={emotion} />
-        </div>
-    )
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
