73b4cb966fb199d937fcfa63ee3c2e5d4e847c8c
Alexander Reyes <alexander_reyes@ucsb.edu>
2024-08-17 14:43:22 -0700

    Renamed detour.py to emotext.py for better name. Commented test code for rn. Work on playing with someone's spotify playlists.

--- ML/detour.py
+++
@@ -1,8 +0,0 @@
-import text2emotion as emo
-with open('lyrics3.txt', 'r') as file:
-    # Read the entire contents of the file as a string
-    file_contents = file.read()
-
-# file_contents now holds the entire content of the file as a string
-test = file_contents
-print(emo.get_emotion(test))
