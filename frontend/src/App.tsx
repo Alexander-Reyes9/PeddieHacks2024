f82aa9a56a4323908decd1c6979f2d1a8d3bbb5b
bakerbook <bakerjbook@gmail.com>
2024-08-18 02:00:18 -0700

    Make front end pretty much ready

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import spotifyLogo from './assets/spotify_logo.png'
 import './App.css'
 
-async function fetchApi(playlistLink: string) {
+async function fetchApi() {
     const response = await fetch("get_playlist_emotion", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ playlistLink }),
+        //body: JSON.stringify({ playlistLink }),
     });
     const data = await response.json();
     return data.playlists;
@@ -25,26 +25,21 @@ function Playlist({ name, songList}: { name: string, songList: Array<string>}){
     )
 }
 
-type PlaylistType = {
-    name: string,
-    songList: Array<string>
-}
+
 
 function App() {
-    const [playlists, setPlaylists] = useState<Array<PlaylistType> | null>(null)
-    const playlistElements = playlists?.map(playlist => { <Playlist name={playlist.name} songList={playlist.songList} /> })
+    const [playlists, setPlaylists] = useState<any>(null)
 
     return (
         <div className="grid place-items-center">
             <img className="logo select-none" src={spotifyLogo}></img>
             <h1 className="text-5xl font-bold">Song Emotion Website</h1>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 m-5 rounded" onClick={async () => {
-                let input = document.getElementById("playlistLink") as HTMLInputElement
-                setPlaylists(await fetchApi(input.value))
+                setPlaylists(await fetchApi())
             }}>Generate emotion playlists</button>
             <div className="">
                 <ul className="grid gap-4 grid-cols-4">
-                    {playlistElements}
+                    {playlists?.map((playlist: any) => { <Playlist name={playlist.name} songList={playlist.songList} /> })}
                     <Playlist name="Sad playlist" songList={['Test song 1', 'Test song 2', 'Test song 3']}/>
                     <Playlist name="Happy playlist" songList={['Test song 1', 'Test song 2', 'Test song 3']}/>
                     <Playlist name="IDK playlist" songList={['Test song 1', 'Test song 2', 'Test song 3']}/>
