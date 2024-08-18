964c75c070bf9b0c07b132adea1e66c72970cb59
bakerbook <bakerjbook@gmail.com>
2024-08-17 23:03:44 -0700

    Add endpoint to get all songs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,15 @@ import spotifyLogo from './assets/spotify_logo.png'
 import './App.css'
 
 async function fetchApi(playlistLink: string) {
-    return playlistLink
+    const response = await fetch("get_playlist_emotion", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ playlistLink }),
+    });
+    const data = await response.json();
+    return data.emotion;
 }
 
 function EmotionDisplay({ emotion }: { emotion: string | null }){
@@ -25,6 +33,7 @@ function App() {
         <div className="grid place-items-center">
             <img className="logo select-none" src={spotifyLogo}></img>
             <h1>Song Emotion Website</h1>
+            <input type="text" id="playlistLink" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 m-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Playlist link" />
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={async () => {
                 let input = document.getElementById("playlistLink") as HTMLInputElement
                 setEmotion(await fetchApi(input.value))
--- spotifyAuth/testServer.py
+++ spotifyAuth/testServer.py
@@ -3,6 +3,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
+import json
 load_dotenv()
 
 app = Flask(__name__, static_folder="../frontend/dist/assets")
@@ -30,22 +31,29 @@ def callback():
     # Use the token to access Spotify data
     sp = spotipy.Spotify(auth=token_info["access_token"], )
 
-    # Get the current user information
-    current_user = sp.current_user()
-    playlists = sp.current_user_playlists()
-
-    # Iterate through songs and display them
-    #iterate_songs(sp, playlists)
-
     return send_file("../frontend/dist/index.html")
 
+@app.route("/get_playlist_emotion", methods=["POST"])
+def get_playlist_emotion():
+    code = request.referrer.split("code=")[1]
+    token_info = sp_oauth.get_access_token(code)
+    sp = spotipy.Spotify(auth=token_info["access_token"])
+    playlists = sp.current_user_playlists()
+
+    print(token_info)
+    songs = iterate_songs(sp, playlists)
+    print(len(songs))
+    return json.dumps({
+        "emotion": songs
+    })
+
 def iterate_songs(sp, playlists):
+    all_tracks = []
     for playlist in playlists['items']:
         playlist_name = playlist['name']
         playlist_id = playlist['id']
 
         print(f"Playlist: {playlist_name}")
-        
         # Get all tracks in the playlist
         tracks = sp.playlist_tracks(playlist_id)
         
@@ -55,7 +63,8 @@ def iterate_songs(sp, playlists):
             artist_names = ', '.join([artist['name'] for artist in track_info['artists']])
             album_name = track_info['album']['name']
             
-            print(f"Song: {song_name} - Artists: {artist_names} - Album: {album_name}")
+            #print(f"Song: {song_name} - Artists: {artist_names} - Album: {album_name}")
+            all_tracks.append(song_name)
         
         # Handle pagination for playlists with more than 100 tracks
         while tracks['next']:
@@ -66,7 +75,9 @@ def iterate_songs(sp, playlists):
                 artist_names = ', '.join([artist['name'] for artist in track_info['artists']])
                 album_name = track_info['album']['name']
                 
-                print(f"Song: {song_name} - Artists: {artist_names} - Album: {album_name}")
+                #print(f"Song: {song_name} - Artists: {artist_names} - Album: {album_name}")
+                all_tracks.append(song_name)
 
+    return all_tracks
 if __name__ == '__main__':
     app.run(port=8888, debug=True)
